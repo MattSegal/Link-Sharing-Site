129aba43c656f14d2808c556d229ac7ed68f0c8e
Matthew Segal <mattdsegal@gmail.com>
2020-07-19 10:46:39 +1000

    Reduce gunicorn workers

--- app/start-web.sh
+++ app/start-web.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "Starting links app as `whoami`" && \
-echo "Running migrations" && \
-./manage.py migrate && \
-mkdir -p /var/log/gunicorn && \
-echo "Starting gunicorn" && \
+set -e
+echo "Running migrations"
+./manage.py migrate
+
+mkdir -p /var/log/gunicorn
+echo "Starting gunicorn"
 gunicorn links.wsgi:application \
   --name links \
-  --workers 3 \
+  --workers 1 \
+  --threads 2 \
   --bind 0.0.0.0:8001 \
   --capture-output \
   --log-level info \
